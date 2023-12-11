1d62ffd52beb87d7762c3ddf179aaf0e1f79db8e
MaxenceMahieux <pro.maxencemahieux@gmail.com>
2023-12-11 09:59:46 +0100

    Init

---
+++ MegaCastings.DBLib/Class/MegaProductionContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaCastings.DBLib.Class;
+
+public partial class MegaProductionContext : DbContext
+{
+    public MegaProductionContext()
+    {
+    }
+
+    public MegaProductionContext(DbContextOptions<MegaProductionContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=qwy.fr;database=MegaProduction;user=evan;password=qwy44*", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.21-mariadb"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_general_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11) unsigned")
+                .HasColumnName("id");
+            entity.Property(e => e.Nom)
+                .HasColumnType("text")
+                .HasColumnName("nom");
+            entity.Property(e => e.Prenom)
+                .HasColumnType("text")
+                .HasColumnName("prenom");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MegaCastings.DBLib/Class/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MegaCastings.DBLib.Class;
+
+public partial class User
+{
+    public uint Id { get; set; }
+
+    public string? Nom { get; set; }
+
+    public string? Prenom { get; set; }
+}
