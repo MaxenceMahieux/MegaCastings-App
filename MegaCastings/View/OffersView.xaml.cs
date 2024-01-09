5d6210b614e211956c9413baf4dd2265c49103b5
Maxence Mahieux <pro.maxencemahieux@gmail.com>
2024-01-09 21:11:58 +0100

    Mise en place de la page OfferView + Delete

--- MegaCastings/View/OffersView.xaml.cs
+++ MegaCastings/View/OffersView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MegaCastings.DBLib.Class;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +18,60 @@ using System.Windows.Shapes;
 namespace MegaCastings.View
 {
     /// <summary>
-    /// Logique d'interaction pour OffersView.xaml
+    /// si tu passe par la j'ai fini tu peux push sur git
     /// </summary>
     public partial class OffersView : Page
     {
+        public ObservableCollection<Announce> allOffer { get; set; }
+
+        public Announce? SelectedOffer { get; set; }
         public OffersView()
         {
+
             InitializeComponent();
+
+            this.DataContext = this;
+
+
+            using (MegaProductionContext allOffers = new())
+            {
+                allOffer = new ObservableCollection<Announce>(allOffers.Announces.ToList());
+            }
+
+            var columnsToDisplay = new Dictionary<string, string>
+            {
+                { "Title", "Titre" },
+                { "Content", "Description" },
+                { "Bigcategoryid", "Catégorie" },
+                { "Subcategoryid", "Sous Catégorie" },
+                { "Datetime", "Date" },
+                { "Contracttypeid", "Type de contract" },
+                { "Hourlyrate", "€/H" },
+            };
+
+            foreach (var column in columnsToDisplay)
+            {
+                DataGridTextColumn dataColumn = new DataGridTextColumn();
+                dataColumn.Header = column.Value;
+                dataColumn.Binding = new Binding(column.Key);
+                offersdatagrid.Columns.Add(dataColumn);
+            }
+
+            offersdatagrid.AutoGenerateColumns = false;
+
+        }
+
+        private void Button_RemovePartner(object sender, RoutedEventArgs e)
+        {
+            if (this.allOffer != null && SelectedOffer != null)
+            {
+                using (MegaProductionContext context = new())
+                {
+                    context.Announces.Remove(SelectedOffer);
+                    context.SaveChanges();
+                    this.allOffer.Remove(SelectedOffer);
+                }
+            }
         }
     }
 }
